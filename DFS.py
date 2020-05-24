45b033655fbb978f7f99b6e54bef711b97ac4ff9
Tanishi Srivastava <42238494+TanishiSrivastava@users.noreply.github.com>
2020-05-24 12:46:52 +0530

    Create DFS.py

---
+++ DFS.py
@@ -0,0 +1,17 @@
+q=[]
+def dfs(s,ss,visited):
+    print(ss,end=' ')
+    for i in s[ss]:
+        if visited[i]==False:
+            visited[i]=True
+            dfs(s,i,visited)
+n,m=map(int,input().split())
+s=[[] for i in range(n+1)]
+for i in range(m):
+    x,y=map(int,input().split())
+    s[x].append(y)
+    s[y].append(x)
+k=int(input())
+visited=[False]*(n+1)
+visited[k]=True
+dfs(s,k,visited)
