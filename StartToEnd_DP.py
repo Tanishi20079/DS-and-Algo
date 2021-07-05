a50abbf0569b4b2a99184c7f586315c66fc959e7
Tanishi Srivastava <42238494+Tanishi20079@users.noreply.github.com>
2021-07-05 18:48:49 +0530

    Create StartToEnd_DP.py

---
+++ StartToEnd_DP.py
@@ -0,0 +1,26 @@
+#no. of ways to reach bottom right from top left with only right or down moves.
+# "#" is the blocked way and "." is clear path
+
+
+n,m=map(int,input().split())
+l=[]
+for i in range(n):
+    # p=input()
+    p=[i for i in input()]
+    l.append(p)
+dp=[[0]*(m)]*(n)
+dp[n-1][m-1]=1
+for i in range(n-1,-1, -1):
+    for j in range(m-1,-1,-1):
+        if i==n-1 and j==m-1:
+            continue
+        if l[i][j]=='#':
+            dp[i][j]=0
+        else:
+            s1=0 if i==n-1 else dp[i+1][j]
+            s2=0 if j==m-1 else dp[i][j+1]
+            dp[i][j]=s1+s2
+print(dp[0][0])
+            
+            
+            
