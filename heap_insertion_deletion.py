5c3a85fd7286f37a75e64e67b6b4cdb8682eb624
Tanishi Srivastava <42238494+Tanishi20079@users.noreply.github.com>
2021-07-07 19:13:56 +0530

    Create heap_insertion_deletion.py

---
+++ heap_insertion_deletion.py
@@ -0,0 +1,37 @@
+#max heap
+
+heap=[]
+def upheapify(heap,idx):
+    if idx==0:return 
+    pi=(idx-1)//2
+    if heap[pi]<heap[idx]:
+        heap[idx],heap[pi]=heap[pi],heap[idx]
+        upheapify(heap, pi)
+    else:
+        return 
+    
+    
+def downheapify(heap,i):
+    li,ri=2*i+1,2*i+2
+    if li>=len(heap) and ri>=len(heap):return
+    max_idx=i
+    if li<len(heap) and heap[li]>heap[max_idx]:max_idx=li
+    if ri<len(heap) and heap[ri]>heap[max_idx]:max_idx=ri
+    if max_idx==i:return
+    heap[i],heap[max_idx]=heap[max_idx],heap[i]
+    downheapify(heap,max_idx)
+    
+def deletePeek(heap):
+    heap[0],heap[len(heap)-1]=heap[len(heap)-1],heap[0]
+    heap.pop()
+    downheapify(heap,0)
+    
+
+n=int(input())
+l=list(map(int,input().split()))
+for i in range(n):
+    heap.append(l[i])
+    upheapify(heap,i)
+print(heap)
+deletePeek(heap)
+print(heap)
